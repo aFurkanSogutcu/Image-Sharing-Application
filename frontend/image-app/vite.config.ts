de7980fa67b0743fafac04d377a68929b5938623
aFurkan <nikonia19crm@gmail.com>
2026-01-11 07:28:42 +0300

    kayıt

--- frontend/image-app/vite.config.ts
+++ frontend/image-app/vite.config.ts
@@ -5,15 +5,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // Frontend -> /api/auth/token
-      // Backend   -> /auth/token  (rewrite ile /api kaldırılıyor)
       "/api": {
         target: "http://127.0.0.1:8000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
 
-      // Media aynen kalsın
       "/media": {
         target: "http://127.0.0.1:8000",
         changeOrigin: true,
